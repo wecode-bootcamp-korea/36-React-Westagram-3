4bb927a275385acfca8eead7b94543de4d81b5a7
WH <wonho@WHui-MacBookPro.local>
2022-08-03 18:55:42 +0900

    수정

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from './pages/eunji/Main/Main.js';
-import Login from './pages/eunji/Login/Login.js';
+import MainEunji from './pages/eunji/Main/Main.js';
+import LoginEunji from './pages/eunji/Login/Login.js';
+import MainGiseon from './pages/giseon/Main/Main.js';
+import LoginGiseon from './pages/giseon/Login/Login.js';
+import MainJunho from './pages/junho/Main/Main.js';
+import LoginJunho from './pages/junho/Login/Login.js';
+import MainWonho from './pages/wonho/Main/Main.js';
+import LoginWonho from './pages/wonho/Login/Login.js';
+
 
 const Router = () => {
     return ( 
         <BrowserRouter> 
             <Routes>
-                <Route path='/' element={<Login />} />
-                <Route path='/main' element={<Main />} />
+                <Route path='/loginEunji' element={<LoginEunji />} />
+                <Route path='/mainEunji' element={<MainEunji />} />
+                <Route path='/loginGiseon' element={<LoginGiseon />} />
+                <Route path='/mainGiseon' element={<MainGiseon />} />
+                <Route path='/loginJunho' element={<LoginJunho />} />
+                <Route path='/mainJunho' element={<MainJunho />} />
+                <Route path='/loginWonho' element={<LoginWonho />} />
+                <Route path='/mainWonho' element={<MainWonho />} />
             </Routes>
         </BrowserRouter>
     )
