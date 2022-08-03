25f1449876046eab0bfd2ea24f8df0f99bed29f2
WH <wonho@WHui-MacBookPro.local>
2022-08-03 16:57:52 +0900

    초기세팅 완료

--- src/Router.js
+++ src/Router.js
@@ -1,18 +1,17 @@
-import React from "react";
-import { BrowserRouter,Routes, Route } from "react-router-dom";
-import Main from "./pages/eunji/Main/main.js";
-import Login from "./pages/eunji/Login/login.js";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './pages/eunji/Main/main.js';
+import Login from './pages/eunji/Login/login.js';
 
-
-const Router =() => {
-    return (
-        <BrowserRouter>
+const Router = () => {
+    return ( 
+        <BrowserRouter> 
             <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/main" element={<Main />} />
+                <Route path='/' element={<Login />} />
+                <Route path='/main' element={<Main />} />
             </Routes>
         </BrowserRouter>
     )
-}
+    }
 
 export default Router;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/common.scss'
-import './style/reset.scss';
+import './styles/common.scss';
+import './styles/reset.scss';
 import Router from './Router';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-    <Router/>    
-);
+root.render(<Router />);
