4f14de0c6abcabd53420439e82e63a1d04f0db2a
WH <wonho@WHui-MacBookPro.local>
2022-08-03 16:18:16 +0900

    초기세팅 수정완료

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { BrowserRouter,Routes, Route } from "react-router-dom";
-import Main from "./pages/Main/main";
-import Login from "./pages/Login/login";
+import Main from "./pages/eunji/Main/main.js";
+import Login from "./pages/eunji/Login/login.js";
 
 
 const Router =() => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Main />} />
+                <Route path="/" element={<Login />} />
+                <Route path="/main" element={<Main />} />
             </Routes>
         </BrowserRouter>
     )
